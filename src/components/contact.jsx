ac608741d71de36b3f952213cd5712ac6e650662
phartenfeller <pharti@hotmail.de>
2019-10-12 23:26:14 +0200

    Contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,23 +4,42 @@ const Contact = () => {
   return (
     <div
       style={{
-        background: '(180deg, #000000 0%, #0A130A 100%)',
+        background: 'linear-gradient(rgb(23, 24, 23) 0%, rgb(14, 15, 14) 100%)',
       }}
     >
       <div className="mx-8 py-6">
         <h2 className="text-green-400 text-3xl font-bold mb-2">Contact</h2>
+        <form id="contact">
+          <label htmlFor="email" className="block">
+            <span className="block text-green-500 text-sm mt-4">E-Mail</span>
+            <input
+              type="text"
+              id="email"
+              className="appearance-none bg-white shadow-inner py-2 px-4 rounded-md w-full text-gray-700 focus:bg-gray-200 focus:outline-none"
+            />
+          </label>
+          <label htmlFor="message" className="block">
+            <span className="block text-green-500 text-sm mt-4">Message</span>
+            <textarea
+              id="message"
+              className="h-32 appearance-none bg-white shadow-inner py-3 px-4 rounded-md w-full text-gray-700 focus:bg-gray-200 focus:outline-none"
+            />
+          </label>
+          <div className="text-center mt-4">
+            <button
+              type="button"
+              className="m-auto py-1 px-20 rounded-md shadow-2xl"
+              style={{
+                background: 'linear-gradient(180deg, #04FF78 0%, #00AE50 100%)',
+              }}
+            >
+              <span className="font-bold text-2xl text-white text-shadow-lg">
+                Send
+              </span>
+            </button>
+          </div>
+        </form>
       </div>
-      <form id="contact">
-        <label htmlFor="email">
-          E-Mail
-          <input type="text" id="email" />
-        </label>
-        <label htmlFor="message">
-          Message
-          <input type="textarea" id="message" />
-        </label>
-        <button type="button">Send</button>
-      </form>
     </div>
   )
 }
