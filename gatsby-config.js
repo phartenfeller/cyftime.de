35b21b8c0753b44702682d382b7bedffc62dad03
phartenfeller <pharti@hotmail.de>
2019-10-14 23:32:30 +0200

    copy php and htacces files on buld

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,34 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `php`,
+        path: `${__dirname}/src/php`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-copy-files',
+      options: {
+        source: `${__dirname}/src/php`,
+        destination: './php',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `config`,
+        path: `${__dirname}/src/config`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-copy-files',
+      options: {
+        source: `${__dirname}/src/config`,
+        destination: '.',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
