3454cdabe7bf85c6fd8a7baf70baee491bbe05a1
phartenfeller <pharti@hotmail.de>
2019-11-22 22:36:30 +0100

    projects progress

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
-
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
+import React from 'react'
 
 const Projects = () => {
   const data = useStaticQuery(graphql`
@@ -18,39 +17,54 @@ const Projects = () => {
   `)
 
   return (
-    <div
-      style={{
-        background:
-          'linear-gradient(180deg, #0E100F 0%, #111615 75%, #192723 100%)',
-      }}
-    >
-      <div
-        className="py-6"
-        style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}
-      >
-        <h2 className="mx-8 lg:mx-64 text-green-400 text-3xl font-bold mb-6">
+    <div>
+      <hr className="mx-10 border-opacity-16 my-8" />
+      <div style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
+        <h2 className="mx-8 lg:mx-64 text-green-200 text-3xl font-bold mb-8">
           Projects
         </h2>
-        <div className="m-auto w-2/3 lg:w-1/3">
-          <Link to="/bokassa/">
+        <Link to="/bokassa/">
+          <div className="m-auto mx-12 rounded-xl shadow-xl bg-opacity-10 pb-6">
+            <div className="text-gray-300 text-3xl pt-3 pl-5">Bokassa</div>
             <Img
               fluid={data.image.childImageSharp.fluid}
               alt="cyf bokassa ep cover"
-              className="m-auto shadow-2xl"
+              className="mx-6 rounded-xl"
             />
-          </Link>
-          <div className="text-green-100 font-bold text-4xl mt-4">
-            Bokassa EP
           </div>
-          <Link to="/bokassa/">
-            <button
-              type="button"
-              className="text-green-300 hover:text-500 font-bold focus:outline-none"
-            >
-              more...
-            </button>
-          </Link>
+        </Link>
+        <div>
+          <svg
+            className="w-2 h-2 inline-block"
+            viewBox="0 0 13 13"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="6.5" cy="6.5" r="6.5" fill="white" fillOpacity="0.33" />
+          </svg>
+          <svg
+            className="w-2 h-2 inline-block"
+            viewBox="0 0 13 13"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="6.5" cy="6.5" r="6.5" fill="white" fillOpacity="0.17" />
+          </svg>
+          <svg
+            className="w-2 h-2 inline-block"
+            viewBox="0 0 13 13"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="6.5" cy="6.5" r="6.5" fill="white" fillOpacity="0.17" />
+          </svg>
         </div>
+
+        <Link to="/projects/">
+          <button
+            type="button"
+            className="text-white bg-green-600 rounded-xl py-1 px-3 hover:text-500 font-bold focus:outline-none"
+          >
+            All Projects
+          </button>
+        </Link>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'opacity-10': 'rgba(255, 255, 255, 0.10);',
+        'opacity-16': 'rgba(255, 255, 255, 0.16);',
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
